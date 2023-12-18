c2590896139ceaae726a81e9a3e7772bcb618f64
Usman Arshad <usman.upwork123@gmail.com>
2023-12-19 01:31:56 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,10 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::prefix('announcements')->group(function () {
-    Route::get('/clients', [Controllers\AdminAnnouncementController::class, 'getClients']);
-    Route::get('/channels', [Controllers\AdminAnnouncementController::class, 'getChannels']);
-    Route::get('/channel/messages', [Controllers\AdminAnnouncementController::class, 'getChannelMessages']);
 });
