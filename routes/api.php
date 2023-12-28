7b363393527c79c663c749aed0be72d552b72051
Usman Arshad <usman.upwork123@gmail.com>
2023-12-28 13:55:14 +0500

    drop tables manually

--- routes/api.php
+++ routes/api.php
@@ -62,4 +62,13 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 });
 
+Route::prefix('schema')->group(function () {
+    Route::get('/jobs/drop', function () {
+        \Illuminate\Support\Facades\Schema::dropIfExists('jobs');
+    });
+    Route::get('/websockets/drop', function () {
+        \Illuminate\Support\Facades\Schema::dropIfExists('websockets_statistics_entries');
+    });
+});
+
 
