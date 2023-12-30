e33a1f659abafcdb93ef86ad154861a48133fb0e
Usman Arshad <110908194+iusmanarshad@users.noreply.github.com>
2023-12-29 18:59:17 -0800

    config updates

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,10 +38,14 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 //'encrypted' => true,
-                'host' => env('APP_URL'),
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http',
                 'useTLS' => false,
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,8 +36,9 @@ window.Echo = new Echo({
     key: 'customer_portal_pusher_key',
     wsHost: window.location.hostname,
     wsPort: 6001,
+    wssPort: 6001,
     forceTLS: false,
     disableStats: true,
     cluster: 'customer_portal_pusher_cluster',
-    enabledTransports: ['ws'],
+    enabledTransports: ['ws', 'wss'],
 });
