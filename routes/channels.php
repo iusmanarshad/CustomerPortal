3e31f2c4efd90dccd0c86001c4129cabe7d5c600
Usman Arshad <usman.upwork123@gmail.com>
2024-01-03 02:15:31 +0500

    Update channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    if ($user->id == $id)
+        return ['id' => $user->id, 'first_name' => $user->first_name, 'last_name' => $user->last_name, 'email' => $user->email];
+//    return (int) $user->id === (int) $id;
 });
 
 Broadcast::channel('message', function ($user) {
