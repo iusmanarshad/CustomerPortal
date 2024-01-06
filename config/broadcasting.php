676082393970045d10915d6e1f2b0892bc7ecad1
Usman Arshad <usman.upwork123@gmail.com>
2024-01-07 02:10:41 +0500

    push the code

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,8 +37,8 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => '127.0.0.1',
-                'port' => 6001,
+                'host' => '*.drummlaw.com',
+                'port' => env('LARAVEL_WEBSOCKETS_PORT'),
                 'scheme' => 'https',
                 'useTLS' => true,
                 'encrypted' => true,
