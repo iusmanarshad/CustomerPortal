70c157a1500c5cd9a3a538139666682158f95936
Usman Arshad <usman.upwork123@gmail.com>
2024-01-08 02:45:01 +0500

    config update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,18 +37,18 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
                 'host' => '127.0.0.1',
                 'port' => env('LARAVEL_WEBSOCKETS_PORT'),
-                'scheme' => 'http',
-                //'scheme' => 'https',
-                //'useTLS' => true,
+                'scheme' => env('PUSHER_SCHEME'),
+                'useTLS' => false,
+                //'encrypted' => true,
                 /*'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
                 ],*/
             ],
             'client_options' => [
+                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
                 //'verify' => false
             ],
         ],
