7c0cee1ae6a1d9381253a557d8ec608c7c3667f4
Usman Arshad <usman.upwork123@gmail.com>
2024-01-07 23:05:19 +0500

    Update channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -17,17 +17,14 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('message.{id}', function ($user) {
-    if ($user->id == $id)
-        return ['id' => $user->id, 'first_name' => $user->first_name, 'last_name' => $user->last_name, 'email' => $user->email];
+Broadcast::channel('message', function ($user) {
+    return \Illuminate\Support\Facades\Auth::check();
 });
 
-Broadcast::channel('announcement.{id}', function ($user) {
-    if ($user->id == $id)
-        return ['id' => $user->id, 'first_name' => $user->first_name, 'last_name' => $user->last_name, 'email' => $user->email];
+Broadcast::channel('announcement', function ($user) {
+    return \Illuminate\Support\Facades\Auth::check();
 });
 
-Broadcast::channel('chat.{id}', function ($user) {
-    if ($user->id == $id)
-        return ['id' => $user->id, 'first_name' => $user->first_name, 'last_name' => $user->last_name, 'email' => $user->email];
+Broadcast::channel('chat', function ($user) {
+    return \Illuminate\Support\Facades\Auth::check();
 });
