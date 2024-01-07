7f7bf184d509989a8d6feb398047e5f12ac1e82c
Usman Arshad <usman.upwork123@gmail.com>
2024-01-08 01:39:24 +0500

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,7 +36,7 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     encrypted: true,
-    //wsHost: window.location.hostname,
+    wsHost: window.location.hostname,
     wsPort: 6002,
     wssPort: 6002,
     forceTLS: false,
