f2beb50dfbb74286be0406fad45940bd6f1f8d13
Usman Arshad <110908194+iusmanarshad@users.noreply.github.com>
2024-01-08 13:43:28 +0500

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('APP_URL'),
+                'host' => env('PUSHER_HOST'),
                 'port' => env('LARAVEL_WEBSOCKETS_PORT'),
                 'scheme' => env('PUSHER_SCHEME'),
-                'useTLS' => false,
+                'useTLS' => env('PUSHER_USE_TLS'),
                 //'encrypted' => true,
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
