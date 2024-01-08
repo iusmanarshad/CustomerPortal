ca6d27ba6978de04f1115ff75ab6ce3fe99b1ed8
Usman Arshad <usman.upwork123@gmail.com>
2024-01-09 03:35:14 +0500

    update footers

--- resources/views/customer-portal/layouts/components/footer.blade.php
+++ resources/views/customer-portal/layouts/components/footer.blade.php
@@ -1,8 +1,7 @@
 
-        
+
 			<div class="main-footer">
 				<div class="container-fluid pd-t-0 ht-100p">
-					 Copyright © 2023 <a href="javascript:void(0);" class="tx-primary">zem</a>. Designed with <span class="fa fa-heart tx-danger"></span> by <a href="https://www.spruko.com/" class="tx-primary"> Spruko </a> All rights reserved
+                    Copyright © 2024 Drummlaw. All rights reserved
 				</div>
 			</div>
-        
